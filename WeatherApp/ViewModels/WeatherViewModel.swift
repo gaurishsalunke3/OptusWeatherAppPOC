bc3cc866720f27885bfa773321398049c6c41536
Gaurish Salunke <gaurisharya@gmail.com>
2020-06-17 10:56:03 +0530

    Created a ViewModel class WeatherViewModel for the CityListView

---
+++ WeatherApp/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by Gaurish Salunke on 6/17/20.
+//  Copyright © 2020 Gaurish Salunke. All rights reserved.
+//
+
+import Foundation
+
+final class WeatherViewModel {
+    var cities: [Weather]
+    
+    init() {
+        
+    }
+}
