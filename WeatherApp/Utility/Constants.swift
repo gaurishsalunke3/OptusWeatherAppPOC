b5024d65c75637e8cf5dc64064a1e4f012a463a1
Gaurish Salunke <gaurisharya@gmail.com>
2020-06-18 17:07:31 +0530

    Removed unwanted code from Constants file.

--- WeatherApp/Utility/Constants.swift
+++ WeatherApp/Utility/Constants.swift
@@ -27,31 +27,4 @@ struct Constants {
         
         static let weatherIconURL = "http://openweathermap.org/img/wn/"
     }
-    
-    struct WeatherIcons {
-        func getWeatherIcon(icon: String) -> UIImage {
-            switch icon {
-            case "01d": return UIImage(systemName: "sun.max")!
-            case "01n": return UIImage(systemName: "moon")!
-            case "02d": return UIImage(systemName: "cloud.sun.max")!
-            case "02n": return UIImage(systemName: "cloud.moon")!
-            case "03d": return UIImage(systemName: "cloud")!
-            case "03n": return UIImage(systemName: "cloud")!
-            case "04d": return UIImage(systemName: "smoke")!
-            case "04n": return UIImage(systemName: "smoke")!
-            case "09d": return UIImage(systemName: "cloud.rain")!
-            case "09n": return UIImage(systemName: "cloud.rain")!
-            case "10d": return UIImage(systemName: "cloud.sun.rain")!
-            case "10n": return UIImage(systemName: "cloud.moon.rain")!
-            case "11d": return UIImage(systemName: "cloud.bolt")!
-            case "11n": return UIImage(systemName: "cloud.bolt")!
-            case "13d": return UIImage(systemName: "snow")!
-            case "13n": return UIImage(systemName: "snow")!
-            case "50d": return UIImage(systemName: "wind")!
-            case "50n": return UIImage(systemName: "wind")!
-                
-            default: return UIImage(systemName: "sun.max")!
-            }
-        }
-    }
 }
